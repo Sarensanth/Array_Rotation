25df24a28fd717f6f64967cdb82d2c806d865baf
Sarensanth <sarensanth2002@gmail.com>
2023-07-12 16:34:42 +0530

    new update

--- Array Rotation/Array_rotation.py
+++ Array Rotation/Array_rotation.py
@@ -1,31 +1,15 @@
-def rotation(array,k):
-    size=len(array)
-    if k>size:
-        k=k%size
-    
-    i=0
-    j=size-1
+def rotation(array,i,j):
     while i<j:
         array[i],array[j]=array[j],array[i]
         i+=1
         j-=1
-
-    i=0
-    j=k-1
-    while i<j:
-        array[i],array[j]=array[j],array[i]
-        i+=1
-        j-=1
-
-    i=k
-    j=size-1
-    while i<j:
-        array[i],array[j]=array[j],array[i]
-        i+=1
-        j-=1
-
-    return array
 
 array=list(map(int,input().split()))
 k=int(input())
-print(rotation(array,k))
+size=len(array)
+if k>size:
+    k=k%size
+rotation(array,0,size-1)
+rotation(array,0,k-1)
+rotation(array,k,size-1)
+print(array)
